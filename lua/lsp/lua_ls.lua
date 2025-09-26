8fcce6ec2bd79880f89443ea7022baaff3c71188
Bert Verbessem <bverbessem@gmail.com>
2025-09-26 16:28:52 +0200

    fix lua_ls undefined global "vim", "Snacks"

--- lua/lsp/lua_ls.lua
+++ lua/lsp/lua_ls.lua
@@ -4,20 +4,37 @@
 --   > github: https://github.com/LuaLS/lua-language-server
 -- ================================================================================================
 
+-- luacheck: globals vim Snacks
+
 vim.lsp.config("lua_ls", {
     capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            local luarc = vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc")
+            if path ~= vim.fn.stdpath("config") and luarc then
+                return
+            end
+        end
+    end,
     settings = {
         Lua = {
-            diagnostics = {
-                globals = { "vim", "Snacks" },
+            runtime = {
+                version = "LuaJIT",
+                path = { "lua/?.lua", "lua/?/init.lua" },
             },
+            diagnostics = { globals = { "vim", "Snacks" } },
             workspace = {
                 library = {
-                    vim.fn.expand("$VIMRUNTIME/lua"),
-                    vim.fn.expand("$XDG_CONFIG_HOME") .. "/nvim/lua",
+                    vim.env.VIMRUNTIME,
+                    vim.fn.stdpath("config") .. "/lua",
+                    "${3rd}/luv/library",
                 },
+                checkThirdParty = false,
             },
+            telemetry = { enable = false },
         },
     },
 })
+
 vim.lsp.enable("lua_ls")
