d480e53155d940b23dc8c023ff680f11edf63a26
Bert Verbessem <bverbessem@gmail.com>
2026-03-08 10:23:07 +0100

    add other things from snacks

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -208,55 +208,55 @@ return {
         --         desc = "Recent",
         --     },
         --     -- git
-        --     {
-        --         "<leader>gb",
-        --         function()
-        --             require("snacks").picker.git_branches()
-        --         end,
-        --         desc = "Git Branches",
-        --     },
-        --     {
-        --     "<leader>gl",
-        --     function()
-        --         require("snacks").picker.git_log()
-        --     end,
-        --     desc = "Git Log",
-        -- },
-        -- {
-        --     "<leader>gL",
-        --     function()
-        --         require("snacks").picker.git_log_line()
-        --     end,
-        --     desc = "Git Log Line",
-        -- },
-        --     {
-        --         "<leader>gs",
-        --         function()
-        --             require("snacks").picker.git_status()
-        --         end,
-        --         desc = "Git Status",
-        --     },
-        --     {
-        --         "<leader>gS",
-        --         function()
-        --             require("snacks").picker.git_stash()
-        --         end,
-        --         desc = "Git Stash",
-        --     },
-        --     {
-        --         "<leader>gd",
-        --         function()
-        --             require("snacks").picker.git_diff()
-        --         end,
-        --         desc = "Git Diff (Hunks)",
-        --     },
-        --     {
-        --         "<leader>gf",
-        --         function()
-        --             require("snacks").picker.git_log_file()
-        --         end,
-        --         desc = "Git Log File",
-        --     },
+        {
+            "<leader>gb",
+            function()
+                require("snacks").picker.git_branches()
+            end,
+            desc = "Git Branches",
+        },
+        {
+            "<leader>gl",
+            function()
+                require("snacks").picker.git_log()
+            end,
+            desc = "Git Log",
+        },
+        {
+            "<leader>gL",
+            function()
+                require("snacks").picker.git_log_line()
+            end,
+            desc = "Git Log Line",
+        },
+        {
+            "<leader>gs",
+            function()
+                require("snacks").picker.git_status()
+            end,
+            desc = "Git Status",
+        },
+        {
+            "<leader>gS",
+            function()
+                require("snacks").picker.git_stash()
+            end,
+            desc = "Git Stash",
+        },
+        {
+            "<leader>gd",
+            function()
+                require("snacks").picker.git_diff()
+            end,
+            desc = "Git Diff (Hunks)",
+        },
+        {
+            "<leader>gf",
+            function()
+                require("snacks").picker.git_log_file()
+            end,
+            desc = "Git Log File",
+        },
         --     -- Grep
         --     {
         --         "<leader>sb",
