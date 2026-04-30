27044b4a50ebd884dab7926987b513fe3367ec94
Bert Verbessem <bverbessem@gmail.com>
2026-04-30 14:45:14 +0200

    pyenv globals to get correct python

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -5,3 +5,10 @@
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- pyenv shims (zshrc lazy-loads pyenv, so shims aren't in PATH at nvim launch)
+local pyenv_root = vim.env.PYENV_ROOT or (vim.env.HOME .. "/.pyenv")
+local pyenv_shims = pyenv_root .. "/shims"
+if vim.uv.fs_stat(pyenv_shims) and not vim.env.PATH:find(pyenv_shims, 1, true) then
+    vim.env.PATH = pyenv_shims .. ":" .. vim.env.PATH
+end
